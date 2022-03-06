0ec61809770014d91f162629594d8044f1655092
Ahmed Jadani <ahmed4star@gmail.com>
2022-03-06 17:29:31 +0100

    adding list feature

--- gitscript.sh
+++ gitscript.sh
@@ -6,7 +6,7 @@
 #    By: ahjadani <ahjadani@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/02 13:09:33 by ahjadani          #+#    #+#              #
-#    Updated: 2022/03/03 17:41:17 by ahjadani         ###   ########.fr        #
+#    Updated: 2022/03/06 17:25:15 by ahjadani         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,24 +18,74 @@ echo "   _____ _____ _______    _____  _____ _____  _____ _____ _______
   \_____|_____|  |_|    |_____/ \_____|_|  \_\_____|_|      |_|   "
 
 echo ""
-echo "[ 1 ] Automatic push every N minutes with random commit message"
+echo "[ 1 ] Automatic push every N seconds with random commit message"
 echo "[ 2 ] Normal push with specific commit message"
 echo "[ 3 ] Push whenever you change something in the code"
 cmsg=$(curl -s http://whatthecommit.com/index.txt)
 
 echo -e $(printf "\e[31m Select an option: \e[0m")
+
+listfile () 
+{
+    shopt -s nullglob
+    dir_count=0
+    file_count=0
+
+    traverse()
+    {
+        dir_count=$(($dir_count + 1))
+        local directory=$1
+        local prefix=$2
+
+        local children=($directory/*)
+        local child_count=${#children[@]}
+
+        for idx in "${!children[@]}"; do 
+            local child=${children[$idx]// /\\ }
+        child=${child##*/}
+
+        local child_prefix="│   "
+        local pointer="├── "
+
+        if [ $idx -eq $((child_count - 1)) ]; then
+            pointer="└── "
+            child_prefix="    "
+        fi
+
+        echo "${prefix}${pointer}$child"
+        [ -d "$directory/$child" ] &&
+            traverse "$directory/$child" "${prefix}$child_prefix" ||
+            file_count=$((file_count + 1))
+        done
+    }
+
+    root="."
+    [ "$#" -ne 0 ] && root="$1"
+    echo $root
+
+    traverse $root ""
+    echo
+    echo "$(($dir_count - 1)) directories, $file_count files"
+    shopt -u nullglob
+}
+
 read n 
 if [ $n -eq 1 ]; then 
-    echo "Select the files to track: "
+    echo "Select the files to track: (type /list to list all files)"
     read files
+    if [ $files = "/list" ]; then
+        listfile
+        echo "Select the files to track:"
+        read files
+    fi
     check=$(ls $files | wc -l | xargs)
     if [ $check -eq 0 ]; then
         echo "Error!"
         exit
     fi
-    echo "Push every N minutes:"
+    echo "Push every N seconds:"
     echo -n "N: "
-    read nmin
+    read nsec
     
     while [ 1 ]
     do
@@ -50,8 +100,14 @@ if [ $n -eq 1 ]; then
 	    sleep $nmin
     done
 elif [ $n -eq 2 ]; then
-    echo "Select the files to track: "
+    echo "Select the files to track: (type /list to list all files)"
     read files
+    if [ $files = "/list" ]; then
+        listfile
+        echo "Select the files to track:"
+        read files
+        
+    fi
     check=$(ls $files | wc -l | xargs)
     if [ $check -eq 0 ]; then
         echo "Error!"
@@ -66,8 +122,14 @@ elif [ $n -eq 2 ]; then
         git push
     done
 elif [ $n -eq 3 ]; then
-    echo "Select the files to track: "
+    echo "Select the files to track: (type /list to list all files)"
     read files
+    if [ $files = "/list" ]; then
+        listfile
+        echo "Select the files to track:"
+        read files
+        
+    fi
     check=$(ls $files | wc -l | xargs)
     if [ $check -eq 0 ]; then
         echo "Error!"
