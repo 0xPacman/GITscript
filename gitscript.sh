6417d6771d9f7d256f562e863aa9f0b996899efb
Ahmed Jadani <ahmed4star@gmail.com>
2022-03-11 16:54:20 +0100

    Fixing a bug for ubuntu users

--- gitscript.sh
+++ gitscript.sh
@@ -27,46 +27,7 @@ echo $(printf "\e[31m Select an option: \e[0m")
 
 listfile () 
 {
-    shopt -s nullglob
-    dir_count=0
-    file_count=0
-
-    traverse()
-    {
-        dir_count=$(($dir_count + 1))
-        local directory=$1
-        local prefix=$2
-
-        local children=($directory/*)
-        local child_count=${#children[@]}
-
-        for idx in "${!children[@]}"; do 
-            local child=${children[$idx]// /\\ }
-        child=${child##*/}
-
-        local child_prefix="│   "
-        local pointer="├── "
-
-        if [ $idx -eq $((child_count - 1)) ]; then
-            pointer="└── "
-            child_prefix="    "
-        fi
-
-        echo "${prefix}${pointer}$child"
-        [ -d "$directory/$child" ] &&
-            traverse "$directory/$child" "${prefix}$child_prefix" ||
-            file_count=$((file_count + 1))
-        done
-    }
-
-    root="."
-    [ "$#" -ne 0 ] && root="$1"
-    echo $root
-
-    traverse $root ""
-    echo
-    echo "$(($dir_count - 1)) directories, $file_count files"
-    shopt -u nullglob
+    python -c "$(curl -fsSL https://gist.githubusercontent.com/0xPacman/e9a669bd1fb9c17477370462977c3009/raw/12e9d9a0e92cccaa5af599868156afbfb48ef529/list.py)"
 }
 
 read n 
